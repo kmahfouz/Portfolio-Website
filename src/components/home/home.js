1dbd42df12d5dce7adaedcefca408065cc23887e
Karim Mahfouz <kmahfouz@bsu.edu>
2023-01-02 18:23:58 -0500

    initial commit

---
+++ src/components/home/home.js
@@ -0,0 +1,11 @@
+import "./home.scss"
+
+const Home = () => {
+    return (
+        <div className={"home"}>
+            <p> home page</p>
+            hi
+        </div>
+    )
+}
+export default Home
