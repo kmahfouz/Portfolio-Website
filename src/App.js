54e94a0bdd82a29e0e6e3ebeda4ca445efb8da39
Karim Mahfouz <kmahfouz@bsu.edu>
2023-01-15 18:50:53 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-// import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./components/home/home";
+import Work from "./components/work/work";
+import Contact from "./components/contact/contact";
 
 function App() {
     return (
-        <div className="App">
-            <Home/>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/work" element={<Work/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
 export default App;
-{/*<Work/>*/}
-{/*<Contact/>*/}
